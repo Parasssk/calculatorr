df0631bc9734c629df666f0fab46ff8aae135735
Parasssk <Paraskapoor4321@gmail.com>
2023-03-21 11:02:20 +0530

    modified

---
+++ calculatorr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+   char operator;
+   double num1, num2, result;
+
+   printf("Enter an operator (+, -, *, / ");
+   scanf("%c", &operator);
+
+   switch(operator) {
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+   }
+
+   return 0;
+}
